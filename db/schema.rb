f320dbdb39a6654a2997c9985ec611af633cf220
Marina Borges <mnascimentos11@gmail.com>
2023-08-25 18:25:23 +0100

    dont know

--- db/schema.rb
+++ db/schema.rb
@@ -82,7 +82,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_25_100237) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "user_name"
+    t.string "user_name", default: "", null: false
     t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
