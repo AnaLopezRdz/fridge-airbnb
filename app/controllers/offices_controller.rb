f85e1b1ba5c2f8e4f9bf15d4c5ddd0e9b2fead71
Marina Borges <mnascimentos11@gmail.com>
2023-08-25 09:59:41 +0100

    Pundit

--- app/controllers/offices_controller.rb
+++ app/controllers/offices_controller.rb
@@ -1,20 +1,26 @@
 class OfficesController < ApplicationController
   before_action :set_office, only: [:show, :edit, :update, :destroy]
+  skip_before_action :authenticate_user!, only: [:index]
 
   def index
-    @offices = Office.all
+    @offices = policy_scope(Office)# can delete the word all because you're calling twice (office_police + here)
   end
 
   def show
+    # authorize @office
   end
 
   def new
     @office = Office.new
+    authorize @office
   end
 
   def create
     @office = Office.new(office_params)
     @office.user = current_user
+
+    authorize @office
+
     if @office.save
       redirect_to office_path(@office)
     else
@@ -23,9 +29,11 @@ class OfficesController < ApplicationController
   end
 
   def edit
+    # authorize @office
   end
 
   def update
+    # authorize @office
     if @office.update(office_params)
       redirect_to office_path(@office)
     else
@@ -34,17 +42,19 @@ class OfficesController < ApplicationController
   end
 
   def destroy
+    # authorize @office
     @office.destroy
     redirect_to office_path, status: :see_other
   end
 
   private
+  def set_office
+    @office = Office.find(params[:id])
+    authorize @office
+  end
 
   def office_params
     params.require(:office).permit(:address, :available_dates, :name, :price, :description, :photo)
   end
 
-  def set_office
-    @office = Office.find(params[:id])
-  end
 end
