925b9290bb0427b4f46373536365dc64db737074
Marina Borges <mnascimentos11@gmail.com>
2023-08-29 23:11:30 +0100

    changes

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "offices#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
- resources :offices do
-  resources :bookings, only: [ :index, :create, :update]
-  resources :reviews, only: [:create]
- end
+  resources :offices do
+    resources :bookings, only: [ :index, :create, :update]
+    resources :reviews, only: [:create]
+  end
   resources :bookings, only: :destroy
   resources :reviews, only: :destroy
   # Defines the root path route ("/")
