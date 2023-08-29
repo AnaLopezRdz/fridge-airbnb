5e78c58ce8234ba1156d1cc9b81fae03af3ff5a6
analopezrdz <annalucable@gmail.com>
2023-08-29 12:52:30 +0200

    policy

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,40 @@
+class ReviewsController < ApplicationController
+  before_action :set_review, only: [:destroy]
+
+
+  def new
+    @review = Review.new
+    authorize @review
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.user = current_user
+
+    authorize @review
+
+    if @review.save
+      redirect_to booking_path(@review)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+
+
+  def destroy
+    # authorize @review
+    @review.destroy
+    redirect_to review_path, status: :see_other
+  end
+
+  private
+  def set_review
+    @review = Review.find(params[:id])
+    authorize @review
+  end
+
+  def booking_params
+    params.require(:review).permit(:comment, :stars)
+  end
+end
---
+++ app/policies/review_policy.rb
@@ -0,0 +1,28 @@
+class ReviewPolicy < ApplicationPolicy
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    def resolve
+      scope.where(user: user)
+    end
+
+    def new?
+      return create?
+    end
+
+    def create?
+      return true
+    end
+
+    def edit?
+      return update?
+    end
+
+    def update?
+      return record.user == user
+    end
+
+    def destroy
+      return record.user == user
+    end
+  end
+end
