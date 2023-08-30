8347ad342dfb3ef7a9d56adaf86d3f00f86085a0
analopezrdz <annalucable@gmail.com>
2023-08-30 11:49:44 +0200

    one more color

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "offices#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
- resources :offices do
-  resources :bookings, only: [  :create, :update]
-  resources :reviews, only: [:create]
- end
+  resources :offices do
+    resources :bookings, only: [ :create, :update ]
+    resources :reviews, only: [:create]
+  end
   resources :bookings, only: [ :destroy, :index ]
   resources :reviews, only: :destroy
   # Defines the root path route ("/")
