5d018be5bcd32505feed58c130ff77f168ac95e9
analopezrdz <annalucable@gmail.com>
2023-09-01 20:44:36 +0200

    routes, edit and index not nexted

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Rails.application.routes.draw do
   root to: "offices#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :offices do
-    resources :bookings, only: [:index, :new, :create ]
+    resources :bookings, only: [ :new, :create ]
     resources :reviews, only: [:create]
   end
-  resources :bookings, only: [:show, :edit, :update, :destroy]
+  resources :bookings, only: [:show, :edit, :update, :destroy, :index]
   resources :reviews, only: :destroy
   # Defines the root path route ("/")
   # root "articles#index"
