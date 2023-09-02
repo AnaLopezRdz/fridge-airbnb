a878251ed1b163edb0fe6ca9ce6d6cb6d7ed918a
analopezrdz <annalucable@gmail.com>
2023-09-02 17:24:31 +0200

    scope and controller for booking

--- app/controllers/offices_controller.rb
+++ app/controllers/offices_controller.rb
@@ -3,7 +3,8 @@ class OfficesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
 
   def index
-     @offices = policy_scope(Office)# can delete the word all because you're calling twice (office_police + here)
+    #  @offices = policy_scope(Office)# can delete the word all because you're calling twice (office_police + here)
+    @offices = policy_scope(Office.all)
   end
 
   def show
--- app/policies/office_policy.rb
+++ app/policies/office_policy.rb
@@ -2,11 +2,10 @@ class OfficePolicy < ApplicationPolicy
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
     def resolve
-      # scope.all
-      scope.where(user: user)
+       scope.all
+      # scope.where(user: user)
     end
   end
-
   def show?
     return true
   end
