8c8764383e04086b825b373d88aec3aab79a71af
Marina Borges <mnascimentos11@gmail.com>
2023-09-04 01:02:33 +0100

    Working in progress

--- app/policies/office_policy.rb
+++ app/policies/office_policy.rb
@@ -2,7 +2,7 @@ class OfficePolicy < ApplicationPolicy
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
     def resolve
-      # scope.all
+      scope.all
       scope.where(user: user)
     end
   end
