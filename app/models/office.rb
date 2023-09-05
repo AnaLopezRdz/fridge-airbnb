94202229664177a9636b408c6971067c9a54fa1f
Maria Thorson <maria.thorson7@gmail.com>
2023-09-05 11:43:42 +0200

    search bar function

--- app/models/office.rb
+++ app/models/office.rb
@@ -5,4 +5,12 @@ class Office < ApplicationRecord
   validates :name, :address, :description, :price, presence: true
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
+
+  include PgSearch::Model
+
+  pg_search_scope :search_by_name_and_description,
+    against: [ :name, :description ],
+    using: {
+      tsearch: { prefix: true }
+    }
 end
