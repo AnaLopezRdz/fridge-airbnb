ac735c54f9828a8929cc59da3c4d1f8bf8e43e8a
Marina Borges <mnascimentos11@gmail.com>
2023-09-06 12:51:35 +0100

    Update Gemfile.lock

--- Gemfile
+++ Gemfile
@@ -32,6 +32,7 @@ gem "jbuilder"
 gem "devise"
 gem "pundit"
 gem "geocoder"
+gem "mailcatcher"
 # Use Kredis to get higher-level data types in Redis [https://github.com/rails/kredis]
 # gem "kredis"
 gem "cloudinary"
--- app/mailers/booking_confirmation_mailer.rb
+++ app/mailers/booking_confirmation_mailer.rb
@@ -1,10 +1,10 @@
-# class BookingConfirmationMailer < ApplicationMailer
-#   default from: "officewhereyouare@gmail.com"
+class BookingConfirmationMailer < ApplicationMailer
+  default from: "officewhereyouare@gmail.com"
 
-#   def confirmation_email(user, booking)
-#     @user = user
-#     @office = booking.office
-#     @booking = booking
-#     mail(to: @user.email, subject: "Your booking confirmation")
-#   end
-# end
+  def confirmation_email(user, booking)
+    @user = user
+    @office = booking.office
+    @booking = booking
+    mail(to: @user.email, subject: "Your booking confirmation")
+  end
+end
